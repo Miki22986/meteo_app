9a9b9639dcadb5e9e8fc869b292cd70134ab7119
Miki22986 <elisa.miki@gmail.com>
2025-02-08 11:54:00 +0100

    modifica Aspetto e aggiunta funzioni
    sia html, css e javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+   let apiKey = "8bb47a1aat7f98e60875f1b93o1d6a06";
+   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}`;
+//   axios.get(apiURL).then(show_temperature);
+
 //   function show_temperature(response) {
 //     let temp = Math.round(response.data.temperature.current);
 //     let temperature_where = document.querySelector(
@@ -6,20 +10,18 @@
 //     temperature_where.innerHTML = temp;
 //   }
 
-// function search(event) {
-//   event.preventDefault();
-//   let searchInputElement = document.querySelector("#search-input");
-//   let cityElement = document.querySelector("#current-city");
-//   cityElement.innerHTML = searchInputElement.value;
-//   let apiKey = "8bb47a1aat7f98e60875f1b93o1d6a06";
-//   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}`;
-//   axios.get(apiURL).then(show_temperature);
-// }
+function search(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
 /*Per la data*/
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let day = date.getDay();
+  let mounth_day = date.getDate();
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -40,11 +42,11 @@ function formatDate(date) {
   ];
 
   let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
+  return `${formattedDay} ${mounth_day},  ${hours}:${minutes} `;
 }
 
-// let searchForm = document.querySelector("#search-form");
-// searchForm.addEventListener("submit", search);
+let searchForm = document.querySelector("#city_src");
+searchForm.addEventListener("submit", search);
 
 let currentDateELement = document.querySelector("#data");
 let currentDate = new Date();
