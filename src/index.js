0ce10cf460ad3c9f3dadbcf0498567c8ce08aadd
Miki22986 <elisa.miki@gmail.com>
2025-02-08 21:39:11 +0100

    javascript funzionante

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-
-function search(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function show_temperature(response){
+  let temperature = document.querySelector("#temp_numb");
+  let temp = Math.round(response.data.temperature.current);
+  temperature.innerHTML = temp;
 }
-
-function show_temperature(response) {
-    let temp = Math.round(response.data.temperature.current);
-    alert("temp");
-    //let temperature = document.querySelector("#temp_numb");
-    //temperature.innerHTML = temp;
-  }
 
 function search_city(city){
     let apiKey = "8bb47a1aat7f98e60875f1b93o1d6a06";
     let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiURL).then(show_temperature);
 }
+
+function search(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#search");
+  let nameCity = document.querySelector("#city");
+  nameCity.innerHTML = searchCity.value;
+  search_city(searchCity.value);
+}
+
+let searchForm = document.querySelector("#city_src");
+searchForm.addEventListener("submit", search);
 
 
 /*Per la data*/
@@ -49,10 +51,6 @@ function formatDate(date) {
   return `${formattedDay} ${mounth_day},  ${hours}:${minutes} `;
 }
 
-let searchForm = document.querySelector("#city_src");
-searchForm.addEventListener("submit", search);
-
 let currentDateELement = document.querySelector("#data");
 let currentDate = new Date();
-
 currentDateELement.innerHTML = formatDate(currentDate);
