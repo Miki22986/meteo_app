09389d6b10578e4da65434ce61714b68e52c24ef
Miki22986 <elisa.miki@gmail.com>
2025-02-13 23:46:38 +0100

    aggiornamento js

--- src/meteo_giorni.js
+++ src/meteo_giorni.js
@@ -1,16 +1,19 @@
 let weather_days = document.querySelector("#meteo_giorni");
-let myHTML = `
-    <div class="giorno">Lunedi 
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+    
+let content = ""; 
+
+for (let i = 0; i < 5; i++) { 
+    let day = days[i];
+    let myHTML = `
+    <div class="giorno">${day} 
         <span class ="emoji_day">⛅</span>
         <div class="max_min">
             21°<span> 8°</span>
         </div> 
     </div>`;
-    
-let content = ""; 
-
-for (let i = 0; i < 5; i++) { 
-    content += myHTML; 
-}
+    content += myHTML;
+};
 
 weather_days.innerHTML = content;
