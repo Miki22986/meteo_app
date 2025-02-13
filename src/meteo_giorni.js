6082ba1ed60a10c3a1863e7959d6d007dc65789f
Miki22986 <elisa.miki@gmail.com>
2025-02-14 00:39:22 +0100

    aggiunta api

--- src/meteo_giorni.js
+++ src/meteo_giorni.js
@@ -1,19 +1,26 @@
-let weather_days = document.querySelector("#meteo_giorni");
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+function meteo_futuro(city) {
+  let apiKey = "8bb47a1aat7f98e60875f1b93o1d6a06";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
 
-    
-let content = ""; 
+function displayForecast(){
+   
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    let content = ""; 
 
-for (let i = 0; i < 5; i++) { 
-    let day = days[i];
-    let myHTML = `
-    <div class="giorno">${day} 
-        <span class ="emoji_day">⛅</span>
-        <div class="max_min">
-            21°<span> 8°</span>
-        </div> 
-    </div>`;
-    content += myHTML;
+    for (let i = 0; i < 5; i++) { 
+        let day = days[i];
+        let myHTML = `
+        <div class="giorno">${day} 
+            <span class ="emoji_day">⛅</span>
+            <div class="max_min">
+                21°<span> 8°</span>
+            </div> 
+        </div>`;
+        content += myHTML;
+    }
 };
 
+let weather_days = document.querySelector("#meteo_giorni");
 weather_days.innerHTML = content;
